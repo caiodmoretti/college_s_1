ff8ad7526c94e4e6b137f9fd4ad5ee59c799be9d
Caio Moretti <102165296+caiodmoretti@users.noreply.github.com>
2022-04-19 14:42:41 -0300

    Adding first list of exercices.

---
+++ linguagem_de_programacao/Conversao.java
@@ -0,0 +1,21 @@
+/* Exercício 02
+Programa para conversão de uma medida de metros para centímetros.
+A medida deverá ser lida pelo teclado.
+Após a leitura, a medida convertida deverá ser apresentada na tela.
+*/
+
+import java.util.Scanner ;
+
+class Conversao {
+  public static void main(String[] args) {
+    double metros, centimetros; // Declaração de variáveis
+    Scanner leia = new Scanner ( System .in );
+      
+      System.out.println("Informe a medida em metros.");
+      metros = leia.nextDouble();
+      
+      centimetros = (metros*100); // Conversão da medida para centímetros.
+      
+      System.out.println("A medida informada corresponde a " + centimetros + "cm.");
+      }
+}
